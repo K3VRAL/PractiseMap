cf6d72e1c7052caaa3e34db47b76499b41bce90d
K3VRAL <kevinbestkiller@gmail.com>
2023-01-27 17:43:47 +0100

    Fixed sh issue

--- pracmap.sh
+++ pracmap.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 for arg in "$@"; do
-	if [[ "install" == "$arg" ]]; then
+	if [ "install" = "$arg" ]; then
 		mkdir -p /usr/local/bin/pracmap
 		cp ./src/* /usr/local/bin/pracmap/
 		chmod 755 /usr/local/bin/pracmap/main.rb
 		ln -s /usr/local/bin/pracmap/main.rb /usr/bin/pracmap
 	fi
-	if [[ "uninstall" == "$arg" ]]; then
+	if [ "uninstall" = "$arg" ]; then
 		unlink /usr/bin/pracmap
 		rm -rf /usr/local/bin/pracmap
 	fi
